aff2f3767e923f73deed4937a86e629de4fe161f
sugarflocky <sugarflocky@gmail.com>
2025-03-09 20:41:43 +0300

    config changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,18 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
 import * as process from 'node:process';
 import { UserAccountsModule } from './features/user-accounts/user-accounts.module';
 import { BloggersPlatformModule } from './features/bloggers-platform/bloggers-platform.module';
 import { TestingModule } from './features/testing/testing-module';
 @Module({
   imports: [
-    ConfigModule.forRoot(),
     MongooseModule.forRoot(
       process.env.MONGO_URI ?? 'mongodb://localhost:27017',
     ),
     UserAccountsModule,
     BloggersPlatformModule,
     TestingModule,
+    configModule,
   ],
   controllers: [],
 })
